15a8984f58c833d6cbc966fcc1bbe028909481f9
godrose <godrose@gmail.com>
2016-12-22 10:57:48 +0000

    Removed obsolete files.

--- src/LogoFX.Client.Testing.EndToEnd/FakeData/Shared/IReaderWriter.cs
+++
@@ -1,8 +0,0 @@
-namespace LogoFX.Client.Testing.EndToEnd.FakeData.Shared
-{
-    public interface IReaderWriter
-    {
-        void WriteText(string path, string text);
-        string ReadText(string path);
-    }
-}
--- src/LogoFX.Client.Testing.EndToEnd/FakeData/Shared/ReaderWriterContext.cs
+++
@@ -1,7 +0,0 @@
-namespace LogoFX.Client.Testing.EndToEnd.FakeData.Shared
-{    
-    public static class ReaderWriterContext
-    {
-        public static IReaderWriter Current { get; set; }
-    }
-}
