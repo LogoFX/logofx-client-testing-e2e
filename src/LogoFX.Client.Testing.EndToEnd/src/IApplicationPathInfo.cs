4f92c9b6f83ed390237bc476a07ee08e97bee5b9
Gennady Verdel <godrose@gmail.com>
2018-10-01 08:59:15 +0300

    Added implementation for start client application service

---
+++ src/LogoFX.Client.Testing.EndToEnd/src/IApplicationPathInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace LogoFX.Client.Testing.EndToEnd
+{
+    /// <summary>
+    /// Represents the application path information.
+    /// </summary>
+    public interface IApplicationPathInfo
+    {
+        /// <summary>
+        /// The app's entry point.
+        /// </summary>
+        string Executable { get; }
+
+        /// <summary>
+        /// The path of the app's folder relatively to the test folder.
+        /// </summary>
+        string RelativePath { get; }
+    }
+}
