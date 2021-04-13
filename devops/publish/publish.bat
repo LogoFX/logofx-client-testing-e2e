5b28384fdeb5dd75395198e1694a346eea707bb1
Gennady Verdel <godrose@gmail.com>
2021-04-13 20:22:06 +0300

    chore: bump Attest.*; LogoFX.* deps

--- devops/publish/publish.bat
+++ devops/publish/publish.bat
@@ -1,6 +1,8 @@
 SET package_name=LogoFX.Client.Tests.EndToEnd
 SET package_version=2.2.0-rc2
-cd ../build
+cd ../../src
+nuget restore
+cd ../devops/build
 call build.bat
 cd ../pack
 call ./pack.bat
